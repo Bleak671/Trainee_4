8920c88e3e7d67c77f390bce0549b412a0e42aef
Bleak671 <55623215+Bleak671@users.noreply.github.com>
2021-10-19 14:12:49 +0300

    IM2-8 EF classes added

---
+++ P4/P4/Models/Photos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P4.Models
+{
+    public class Photos
+    {
+        public int PhotoId { get; set; }
+        public string Link { get; set; }
+        public int OwnerId { get; set; }
+        public string Name { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string Hash { get; set; }
+        public DateTime TrashDate { get; set; }
+        public int Views { get; set; }
+        public bool isPublished { get; set; }
+        public bool isTrash { get; set; }
+    }
+}
---
+++ P4/P4/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P4.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public string Login { get; set; }
+        public bool isAdmin { get; set; }
+        public bool isBanned { get; set; }
+    }
+}
